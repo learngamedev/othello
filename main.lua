71920baf4908d2812be12f0f1adad7c12c9b1a24
phansonloc1999 <phansonloc1999@gmail.com>
2019-04-21 20:51:33 +0700

    Initial commit

---
+++ main.lua
@@ -0,0 +1,11 @@
+require("src/Dependencies")
+
+function love.load()
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+    love.mouse.buttonsPressed = {}
+end
---
+++ src/Dependencies.lua
@@ -0,0 +1,4 @@
+require("src/Constants")
+require("src/Utils")
+
+Class = require("lib/class")
